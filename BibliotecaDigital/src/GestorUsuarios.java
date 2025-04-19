8ea8581e965feec6961d120f7d13606e3a8cee0c
celi <celinaguerradiaz@gmail.com>
2025-04-18 21:20:24 -0300

    implemento gestion de usuarios

--- BibliotecaDigital/src/GestorUsuarios.java
+++ BibliotecaDigital/src/GestorUsuarios.java
@@ -2,22 +2,24 @@ import java.util.Map;
 import java.util.HashMap;
 
 public class GestorUsuarios {
-    private Map<Integer, Usuario> usuarios = new HashMap<>();
-    private ServicioNotificaciones notificador; //
+    private Map<String, Usuario> usuarios = new HashMap<>(); //ahora se busca por email, ya que id es int
+    private ServicioNotificaciones notificador;
 
     public GestorUsuarios(ServicioNotificaciones notificador) {
         this.notificador = notificador;
     } //
 
     public void agregarUsuario(Usuario usuario) {
-        usuarios.put(usuario.getId(), usuario);
-
-        String mensaje = "Bienvenido/a " + usuario.getNombre() + " " + usuario.getApellido();//
-        notificador.enviarNotificacion(usuario.getEmail(), mensaje); //
+        usuarios.put(usuario.getEmail(), usuario);
+        notificador.enviarNotificacion("Bienvenido, " + usuario.getNombre() + "!");
     }
 
     public void listarUsuarios() {
         usuarios.values().forEach(System.out::println);
+    }
+
+    public Usuario buscarUsuarioPorEmail(String email) {
+        return usuarios.get(email);
     }
 
 }
