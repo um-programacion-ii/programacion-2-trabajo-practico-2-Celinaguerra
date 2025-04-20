4a345968c44503acb966f4eb64195d0c46652063
celi <celinaguerradiaz@gmail.com>
2025-04-20 19:26:56 -0300

    añado clase prestamo con atributos basicos

---
+++ BibliotecaDigital/src/Prestamo.java
@@ -0,0 +1,37 @@
+import java.time.LocalDate;
+
+public class Prestamo {
+    private Usuario usuario;
+    private RecursoDigital recurso;
+    private LocalDate fechaPrestamo;
+    private LocalDate fechaDevolucion;
+
+    public Prestamo(Usuario usuario, RecursoDigital recurso) {
+        this.usuario = usuario;
+        this.recurso = recurso;
+        this.fechaPrestamo = LocalDate.now();
+        this.fechaDevolucion = fechaPrestamo.plusDays(14);
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public RecursoDigital getRecurso() {
+        return recurso;
+    }
+
+    public LocalDate getFechaPrestamo() {
+        return fechaPrestamo;
+    }
+
+    public LocalDate getFechaDevolucion() {
+        return fechaDevolucionEstimada;
+    }
+
+    @Override
+    public String toString() {
+        return "Préstamo de " + recurso.getTitulo() + " a " + usuario.getNombre() + " (" + usuario.getEmail() + ")" +
+                " - Fecha: " + fechaPrestamo + ", Devolución: " + fechaDevolucion;
+    }
+}
