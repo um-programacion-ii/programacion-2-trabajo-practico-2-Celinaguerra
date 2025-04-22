73a6ca009075d0823b7f100f4c8f52c26ccb0fea
celi <celinaguerradiaz@gmail.com>
2025-04-22 20:31:13 -0300

    creo metodo permitirRenovación

--- BibliotecaDigital/src/AlertaVencimiento.java
+++ BibliotecaDigital/src/AlertaVencimiento.java
@@ -1,9 +1,11 @@
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Scanner;
 
 public class AlertaVencimiento {
 
     private GestorPrestamos gestorPrestamos;
+    private Scanner scanner = new Scanner(System.in);
 
     public AlertaVencimiento(GestorPrestamos gestorPrestamos) {
         this.gestorPrestamos = gestorPrestamos;
@@ -11,15 +13,17 @@ public class AlertaVencimiento {
 
     public void verificarAlertas() {
         List<Prestamo> prestamos = gestorPrestamos.obtenerTodosLosPrestamos();
+        LocalDate hoy = LocalDate.now();
 
         for (Prestamo prestamo : prestamos) {
             LocalDate fechaDevolucion = prestamo.getFechaDevolucion();
-            LocalDate hoy = LocalDate.now();
 
             if (fechaDevolucion.equals(hoy.plusDays(1))) {
                 mostrarAlerta(prestamo, "Su préstamo vence mañana:");
+                permitirRenovacion(prestamo);
             } else if (fechaDevolucion.equals(hoy)) {
                 mostrarAlerta(prestamo, "Su préstamo vence hoy:");
+                permitirRenovacion(prestamo);
             }
         }
     }
@@ -31,5 +35,19 @@ public class AlertaVencimiento {
         System.out.println("Recurso: " + prestamo.getRecurso().getTitulo());
         System.out.println("Fecha de devolución: " + prestamo.getFechaDevolucion());
         System.out.println("============================\n");
+    }
+
+    private void permitirRenovacion(Prestamo prestamo) {
+        System.out.print("¿Desea renovar este préstamo? (s/n): ");
+        String respuesta = scanner.nextLine().trim().toLowerCase();
+
+        if (respuesta.equals("s")) {
+            boolean exito = gestorPrestamos.renovarPrestamo(prestamo);
+            if (exito) {
+                System.out.println("Préstamo renovado.\n");
+            } else {
+                System.out.println("No fue posible renovar el préstamo.\n");
+            }
+        }
     }
 }
