6ac916ed4da8a659ab3b205bad830df82c817e68
celi <celinaguerradiaz@gmail.com>
2025-04-22 22:29:20 -0300

    creo clase alertadisponibilidad, notifica disponibilidad

---
+++ BibliotecaDigital/src/AlertaDisponibilidad.java
@@ -0,0 +1,18 @@
+public class AlertaDisponibilidad {
+    private final GestorReservas gestorReservas;
+    private final ServicioNotificaciones servicioNotificaciones;
+
+    public AlertaDisponibilidad(GestorReservas gestorReservas, ServicioNotificaciones servicioNotificaciones) {
+        this.gestorReservas = gestorReservas;
+        this.servicioNotificaciones = servicioNotificaciones;
+    }
+
+    public void notificarSiHayReserva(RecursoDigital recurso) {
+        Reserva siguiente = gestorReservas.obtenerSiguienteReserva();
+        if (siguiente != null && siguiente.getRecurso().equals(recurso)) {
+            Usuario usuario = siguiente.getUsuario();
+            String mensaje = "El recurso \"" + recurso.getTitulo() + "\" que reservaste está disponible.";
+            servicioNotificaciones.enviarNotificacion(usuario, mensaje);
+        }
+    }
+}
